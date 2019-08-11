5edd743f576c62fcd75b6cbf92c52f458ea761d1
Glitch (lolfg) <none>
2019-08-11 13:17:17 +0000

    🚵🌈 Updated with Glitch

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    TOKEN: 'NjAyMDk5MzUxOTc5ODE5MDI0.XTXg2w.z9JH6Ih96citLNh2uwck06hhYS4',    
+    YT_API_KEY: 'AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8',
+    prefix: '!',
+    devs: ['575791868948774934']
+}
